95d516e71be7589361191ffe6ac9dbe1750c92ee
lonewanderer27 <adrianejames27@gmail.com>
2024-03-18 03:13:28 +0800

    feat: update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jay - Personal Website';
+export const SITE_DESCRIPTION = 'Hello it’s Jay and I’m a Front End Web Developer with a year of hands-on experience designing, developing and implementing web applications and solutions.';
